a7d3c78b7ab51dc3d63fbc93f9469eba7b66667f
ximfem <2744184711@qq.com>
2023-05-06 14:53:01 +0800

    Site updated: 2023-05-06 14:53:00

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "10234",
+    "background": "5",
+    "tmpColor": "00FF00",
+    "tmpSize": "16",
+    "cityColor": "666666",
+    "citySize": "16",
+    "aqiColor": "00FFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "left",
+    "key": "580d1a89163e443aa450621a5c8f9780"
+  }
+}
