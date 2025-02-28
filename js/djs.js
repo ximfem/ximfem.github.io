b33d1aaeef4f6b78db197242c5cf4cae66e5992b
ximfem <2744184711@qq.com>
2025-02-28 19:44:25 +0800

    Site updated: 2025-02-28 19:44:23

--- js/djs.js
+++ js/djs.js
@@ -3,7 +3,7 @@ var newYear = () => {
     clearTimeout(newYearTimer);
     if (!document.querySelector('#newYear')) return;
     // 新年时间戳 and 星期对象
-    let newYear = new Date('2025-01-29 00:00:00').getTime() / 1000,
+    let newYear = new Date('2026-02-17 00:00:00').getTime() / 1000,
         week = { 0: '周日', 1: '周一', 2: '周二', 3: '周三', 4: '周四', 5: '周五', 6: '周六' }
 
     time();
@@ -27,14 +27,14 @@ var newYear = () => {
             document.querySelector('#newYear .newYear-time').innerHTML = '<span class="happyNewYear">新年快乐</span>';
         } else {
             // 大于0则还未过年
-            
+
 
             // 大于一天则直接渲染天数
             if (second > 86400) {
-                document.querySelector('#newYear .title').innerHTML = '距离2025年春节还有：'
+                document.querySelector('#newYear .title').innerHTML = '距离2026年春节还有：'
                 document.querySelector('#newYear .newYear-time').innerHTML = `<span class="day">${Math.ceil(second / 86400)}<span class="unit">天</span></span>`
             } else {
-                document.querySelector('#newYear .title').innerHTML = '2025年春节倒计时：'
+                document.querySelector('#newYear .title').innerHTML = '2026年春节倒计时：'
                 // 小于一天则使用时分秒计时。
                 let h = nol(parseInt(second / 3600));
                 second %= 3600;
@@ -49,7 +49,7 @@ var newYear = () => {
     }
 
     // 元宝飘落
-    jQuery(document).ready(function($) {
+    jQuery(document).ready(function ($) {
         $('#newYear').wpSuperSnow({
             flakes: ['/img/yb1.webp', '/img/yb2.webp', '/img/yb3.webp'],
             totalFlakes: '100',
